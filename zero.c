dd99d4c1a323c340e5a307e19d392e9364466466
Garikinaabhishek <96563962+Garikinaabhishek@users.noreply.github.com>
2021-12-24 13:54:28 +0800

    Add files via upload

---
+++ zero.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+main()
+{
+ 	int number;
+ 	printf("enter a number");
+ 	scanf("%d",&number);
+ 	if(number>0)
+ 	{
+ 		printf("number is positve");
+ 		
+ 		
+	
+	}
+	
+
+	else
+	{
+		printf("number is negative");
+	
+	}
+	
+}
---
+++ zero1 .c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+main()
+{
+	int number;
+	printf("enter a number");
+	scanf("%d",&number);
+	if(number%2==0)
+	{
+		printf("number is even");
+	}
+	else
+	{
+		printf("number is odd");
+		
+	}
+	
+}
