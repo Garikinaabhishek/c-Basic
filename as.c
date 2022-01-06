d16d2c2d49956186bea6e8afc4b07e7f95e76beb
Garikinaabhishek <96563962+Garikinaabhishek@users.noreply.github.com>
2022-01-06 11:02:07 +0530

    Add files via upload

---
+++ as.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{ 
+float a,p,r;
+printf("enter  r");
+scanf("%f", &r);
+a = 3.14*r*r;
+p = 2*3.14*r; 
+printf("area = %f",a);
+printf("\nperimeter = %f",p);
+}
+
+
+ 
