76a480fcadab342dbb4bdf577615453b632afe76
Garikinaabhishek <96563962+Garikinaabhishek@users.noreply.github.com>
2022-01-08 09:51:36 +0530

    Add files via upload

---
+++ number of even odd zero.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+void main()
+{
+	int a,s,e=0,o=0,z=0;
+	scanf("%d",&a);
+	while(a>0)
+	{
+		s=a%10;
+		if(s==0)
+		{
+			z++;
+		}
+		else if(s%2==0)
+		{
+			e++;
+		}
+		else
+		{
+			o++;
+			
+		}
+		a=a/10;
+	}
+	printf("%d %d %d",e,o,z);
+	
+}
